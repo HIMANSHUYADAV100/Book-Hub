7fd9b1c8783293993423aa7ea9b05cb46e57fa80
HIMANSHUYADAV100 <54714942+HIMANSHUYADAV100@users.noreply.github.com>
2021-07-08 16:58:39 +0530

    adapter file created

--- app/src/main/java/com/hiya7/bookhub/fragment/DashboardFragment.kt
+++ app/src/main/java/com/hiya7/bookhub/fragment/DashboardFragment.kt
@@ -1,5 +1,6 @@
 package com.hiya7.bookhub.fragment
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,12 +9,22 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.hiya7.bookhub.R
+import com.hiya7.bookhub.adapter.DashboardRecyclerAdapter
 
 class DashboardFragment : Fragment() {
 
-    lateinit var recyclerDashboard : RecyclerView
+    lateinit var recyclerDashboard: RecyclerView
 
     lateinit var layoutManager: RecyclerView.LayoutManager
+
+    val bookList = arrayListOf(
+        "Harry Potter",
+        "A Brief History of time",
+        "Thunderbird",
+        "The Last Letter"
+    )
+
+    lateinit var recyclerAdapter: DashboardRecyclerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +35,13 @@ class DashboardFragment : Fragment() {
 
         recyclerDashboard = view.findViewById(R.id.recyclerDashboard)
         layoutManager = LinearLayoutManager(activity)
+        recyclerAdapter = DashboardRecyclerAdapter(
+            activity as Context,
+            bookList
+        )
+
+        recyclerDashboard.adapter = recyclerAdapter
+        recyclerDashboard.layoutManager = layoutManager
 
 
         return view
