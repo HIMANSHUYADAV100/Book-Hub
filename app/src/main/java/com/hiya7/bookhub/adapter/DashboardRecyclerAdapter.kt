7a6366ea7dea89e5f6c1891bc03fcf079af3c21c
HIMANSHUYADAV100 <54714942+himanshuyadav100@users.noreply.github.com>
2021-07-09 09:22:26 +0530

    data class implemented

--- app/src/main/java/com/hiya7/bookhub/adapter/DashboardRecyclerAdapter.kt
+++ app/src/main/java/com/hiya7/bookhub/adapter/DashboardRecyclerAdapter.kt
@@ -4,16 +4,19 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.hiya7.bookhub.R
+import com.hiya7.bookhub.model.Book
 
-class DashboardRecyclerAdapter(val context:Context, val itemList : ArrayList<String>, val authList: ArrayList<String>, val priceView: ArrayList<String>) : RecyclerView.Adapter<DashboardRecyclerAdapter.DashboardViewHolder>(){
+class DashboardRecyclerAdapter(val context:Context, val itemList : ArrayList<Book>) : RecyclerView.Adapter<DashboardRecyclerAdapter.DashboardViewHolder>(){
 
     class DashboardViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.txtRecyclerView)
         val authView: TextView = view.findViewById(R.id.txtAuthor)
         val priceView: TextView = view.findViewById(R.id.txtPrice)
+        val imageView: ImageView= view.findViewById(R.id.imgRecyclerView)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DashboardViewHolder {
@@ -28,13 +31,15 @@ class DashboardRecyclerAdapter(val context:Context, val itemList : ArrayList<Str
     }
 
     override fun onBindViewHolder(holder: DashboardViewHolder, position: Int) {
-        val text = itemList[position]
-        val auth = authList[position]
-        val price= priceView[position]
+        val text = itemList[position].bookName
+        val auth = itemList[position].bookAuthor
+        val price= itemList[position].bookCost
+        val image= itemList[position].bookImage
 
         holder.textView.text = text
         holder.authView.text = auth
         holder.priceView.text= price
+        holder.imageView.setImageResource(image)
     }
 
 }
--- app/src/main/java/com/hiya7/bookhub/fragment/DashboardFragment.kt
+++ app/src/main/java/com/hiya7/bookhub/fragment/DashboardFragment.kt
@@ -6,10 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.hiya7.bookhub.R
 import com.hiya7.bookhub.adapter.DashboardRecyclerAdapter
+import com.hiya7.bookhub.model.Book
 
 class DashboardFragment : Fragment() {
 
@@ -53,6 +55,17 @@ class DashboardFragment : Fragment() {
 
     lateinit var recyclerAdapter: DashboardRecyclerAdapter
 
+    val bookInfoList = arrayListOf<Book>(
+        Book("Harry Potter","J.K Rowling","1325","4.5",R.drawable.bmoon),
+        Book("A Brief History of time","Stephan Hawkings","4599","4.7",R.drawable.bmoon),
+        Book("Thunderbird","Iam Fleming","699","4.2",R.drawable.bmoon),
+        Book("The Epic Of Gilgamesh","Gangest Remi","1756","3.2",R.drawable.bmoon),
+        Book("The Divine Comedy","Dante Aleigrihi","950","3.7",R.drawable.bmoon),
+        Book("Things Fall Apart","George Pinglet","899","4.0",R.drawable.bmoon),
+        Book("Fairy tales","Peter Pan","2500","4.1",R.drawable.bmoon),
+        Book("The Last Letter","Enzo Ferrari","6550","4.5",R.drawable.bmoon)
+    )
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -64,14 +77,18 @@ class DashboardFragment : Fragment() {
         layoutManager = LinearLayoutManager(activity)
         recyclerAdapter = DashboardRecyclerAdapter(
             activity as Context,
-            bookList,
-            authorList,
-            priceList
+            bookInfoList
         )
 
         recyclerDashboard.adapter = recyclerAdapter
         recyclerDashboard.layoutManager = layoutManager
 
+        recyclerDashboard.addItemDecoration(
+            DividerItemDecoration(
+                recyclerDashboard.context,
+                (layoutManager as LinearLayoutManager).orientation
+            )
+        )
 
         return view
     }
