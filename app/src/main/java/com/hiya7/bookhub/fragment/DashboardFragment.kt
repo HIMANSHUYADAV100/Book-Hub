ebaf398a5e5774b2558f49835ebd62653e90250d
HIMANSHUYADAV100 <54714942+himanshuyadav100@users.noreply.github.com>
2021-07-10 17:27:12 +0530

    NO INTERNET PROBLEM SOLVED : SETTINGS OR EXIT

--- app/src/main/java/com/hiya7/bookhub/fragment/DashboardFragment.kt
+++ app/src/main/java/com/hiya7/bookhub/fragment/DashboardFragment.kt
@@ -1,15 +1,19 @@
 package com.hiya7.bookhub.fragment
 
+import android.app.Activity
 import android.app.AlertDialog
 import android.app.DownloadManager
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -138,70 +142,97 @@ class DashboardFragment : Fragment() {
 
         val url = "http://13.235.250.119/v1/book/fetch_books/"
 
-        val jsonObjectRequest =
-            object : JsonObjectRequest(Request.Method.GET, url, null, Response.Listener {
-                // handle the resposne
-                println("Response is $it")
+        if (ConnectionManager().checkConnectivity(activity as Context)) {
+            val jsonObjectRequest =
+                object : JsonObjectRequest(Request.Method.GET, url, null, Response.Listener {
+                    // handle the resposne
+                    println("Response is $it")
 
-                val success = it.getBoolean("success")
+                    val success = it.getBoolean("success")
 
-                if (success) {
+                    if (success) {
 
-                    val data = it.getJSONArray("data")
-                    for (i in 0 until data.length()) {
-                        val bookJsonObject = data.getJSONObject(i)
-                        val bookObject = Book(
-                            bookJsonObject.getString("name"),
-                            bookJsonObject.getString("author"),
-                            bookJsonObject.getString("price"),
-                            bookJsonObject.getString("rating"),
-                            bookJsonObject.getString("image")
-                        )
-
-                        bookInfoList.add(bookObject)
-
-                        recyclerAdapter = DashboardRecyclerAdapter(
-                            activity as Context,
-                            bookInfoList
-                        )
-
-                        recyclerDashboard.adapter = recyclerAdapter
-                        recyclerDashboard.layoutManager = layoutManager
-
-                        recyclerDashboard.addItemDecoration(
-                            DividerItemDecoration(
-                                recyclerDashboard.context,
-                                (layoutManager as LinearLayoutManager).orientation
+                        val data = it.getJSONArray("data")
+                        for (i in 0 until data.length()) {
+                            val bookJsonObject = data.getJSONObject(i)
+                            val bookObject = Book(
+                                bookJsonObject.getString("name"),
+                                bookJsonObject.getString("author"),
+                                bookJsonObject.getString("price"),
+                                bookJsonObject.getString("rating"),
+                                bookJsonObject.getString("image")
                             )
-                        )
+
+                            bookInfoList.add(bookObject)
+
+                            recyclerAdapter = DashboardRecyclerAdapter(
+                                activity as Context,
+                                bookInfoList
+                            )
+
+                            recyclerDashboard.adapter = recyclerAdapter
+                            recyclerDashboard.layoutManager = layoutManager
+
+                            recyclerDashboard.addItemDecoration(
+                                DividerItemDecoration(
+                                    recyclerDashboard.context,
+                                    (layoutManager as LinearLayoutManager).orientation
+                                )
+                            )
+
+                        }
+
+                    } else {
+
+                        Toast.makeText(
+                            activity as Context,
+                            "Some Error has Occured",
+                            Toast.LENGTH_SHORT
+                        ).show()
 
                     }
 
-                } else{
 
-                    Toast.makeText(activity as Context, "Some Error has Occured", Toast.LENGTH_SHORT).show()
+                }, Response.ErrorListener {
 
+                    // handle the Errors
+                    println("Error is $it")
+
+                }) {
+
+                    override fun getHeaders(): MutableMap<String, String> {
+
+                        val headers = HashMap<String, String>()
+                        headers["Content-type"] = "application/json"
+                        headers["token"] = "2774371f1180aa"
+
+                        return headers
+                    }
                 }
 
+            queue.add(jsonObjectRequest)
+        } else {
 
-            }, Response.ErrorListener {
+            val dialog2 = AlertDialog.Builder(activity as Context)
 
-                // handle the Errors
-                println("Error is $it")
+            dialog2.setTitle("Error")
+            dialog2.setMessage("Internet Connection is NOT Found")
 
-            }) {
-
-                override fun getHeaders(): MutableMap<String, String> {
-
-                    val headers = HashMap<String, String>()
-                    headers["Content-type"] = "application/json"
-                    headers["token"] = "2774371f1180aa"
-
-                    return headers
-                }
+            dialog2.setPositiveButton("Open Settings") { text, listener ->
+                // open settings (implicit intent
+                val settingsIntent = Intent(Settings.ACTION_WIRELESS_SETTINGS)
+                startActivity(settingsIntent)
+                activity?.finish()
             }
 
-        queue.add(jsonObjectRequest)
+            dialog2.setNegativeButton("Exit") { text, listener ->
+                // close app
+                ActivityCompat.finishAffinity(activity as Activity)
+            }
+
+            dialog2.create()
+            dialog2.show()
+        }
 
         return view
     }
