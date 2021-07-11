7dba0e2739690c8bfa1e082e553413bb56ed6dec
HIMANSHUYADAV100 <54714942+himanshuyadav100@users.noreply.github.com>
2021-07-11 10:12:12 +0530

    DescriptionActivity added

---
+++ app/src/main/java/com/hiya7/bookhub/activity/DescriptionActivity.kt
@@ -0,0 +1,11 @@
+package com.hiya7.bookhub.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DescriptionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_description)
+    }
+}
