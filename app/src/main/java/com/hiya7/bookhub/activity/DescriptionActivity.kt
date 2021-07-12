90e47cd6d02893da19e2c443c1a356887323ca6a
HIMANSHUYADAV100 <54714942+himanshuyadav100@users.noreply.github.com>
2021-07-12 22:34:42 +0530

    Room dependencies added

--- app/src/main/java/com/hiya7/bookhub/activity/DescriptionActivity.kt
+++ app/src/main/java/com/hiya7/bookhub/activity/DescriptionActivity.kt
@@ -1,8 +1,6 @@
 package com.hiya7.bookhub.activity
 
-import android.app.Activity
 import android.app.AlertDialog
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
